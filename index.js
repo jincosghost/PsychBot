976b39cb4f5aa8ec0ca926c58fa042c54f6f0cff
jincosghost <phaposlucl@thraml.com>
2016-10-24 15:05:55 +0100

    3.1.0 Update + add coin flip + some tidying + add uptime check + improved whois readability + added advanced roll and gmroll + fixed blank goal setting

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const botAvatar = settings.avatar;
 const botGame = settings.game;
 const welcomePM = settings.welcomePM.join("\n");
 
+let uptime;
+
 function isMention(usr) {
     if (usr == null || typeof usr == undefined || usr == undefined) {
         return false;
@@ -34,19 +36,22 @@ bot.on("message", (msg) => {
         }
     }
 
-    let input = msg.cleanContent.toUpperCase(); //for case insensitive stuff
+    let input = msg.content.toUpperCase(); //for case insensitive stuff
     var db = firebase.database();
 
     if (msg.content.startsWith(prefix + "help")){
         msg.channel.sendMessage(`\`\`\`Markdown
-#PsychBot\'s Commands\n!help : displays this message.\n\n< Memes >\n!isdoe @user : checks if mentioned user is Doe.\n!whois @user : displays info on the mentioned user.\n!roll # : rolls a die with the specified number of sides.\n!gmroll # : same as roll but the result is sent to you in a DM.\n!yn (question) : will answer your question with yes or no.\n!8ball (question) : will shake a magic 8 ball for you.\n\n< Goals >\n!setgoal (goal) : will set a publically viewable goal.\n!getgoal @user : displays the mentioned user\'s goal.\n!delgoal : will delete your set goal.\n\n< Wellbeing Tracking >\n[Wellbeing scores can be obtained at Jinco\'s Github](http://jincosghost.github.io/permap).\n!setperma 1,2,3,4,5,6,7,8,9 : will store your PERMA scores in this order:\n    [1]: Positive Emotion,\n    [2]: Negative Emotion,\n    [3]: Engagement,\n    [4]: Relationships,\n    [5]: Meaning,\n    [6]: Accomplishment,\n    [7]: Health,\n    [8]: Overall Wellbeing,\n    [9]: Lonliness.\nThis command will also generate a timecode for the stored results so that you can delete them later.\n!getperma @user : displays averages for user\'s stored PERMA scores.\n!delperma (timecode) : will delete the specified set of PERMA scores.\`\`\``);
+#PsychBot\'s Commands\n!help : displays this message.\n!uptime : displays my uptime.\n\n< Memes >\n!isdoe @user : checks if mentioned user is Doe.\n!whois @user : displays info on the mentioned user.\n!roll dice,sides,modifier : rolls dice.\n!gmroll # : same as roll but the result is sent to you in a DM.\n!yn (question) : will answer your question with yes or no.\n!8ball (question) : will shake a magic 8 ball for you.\n!coin : will flip a coin.\n\n< Goals >\n!setgoal (goal) : will set a publically viewable goal.\n!getgoal @user : displays the mentioned user\'s goal.\n!delgoal : will delete your set goal.\n\n< Wellbeing Tracking >\n[Wellbeing scores can be obtained at Jinco\'s Github](http://jincosghost.github.io/permap).\n!setperma 1,2,3,4,5,6,7,8,9 : will store your PERMA scores in this order:\n    [1]: Positive Emotion,\n    [2]: Negative Emotion,\n    [3]: Engagement,\n    [4]: Relationships,\n    [5]: Meaning,\n    [6]: Accomplishment,\n    [7]: Health,\n    [8]: Overall Wellbeing,\n    [9]: Lonliness.\nThis command will also generate a timecode for the stored results so that you can delete them later.\n!getperma @user : displays averages for user\'s stored PERMA scores.\n!delperma (timecode) : will delete the specified set of PERMA scores.\`\`\``);
+    } else if (msg.content.startsWith(prefix + "uptime")) {
+        msg.channel.sendMessage(`${msg.author.username}, I have been online for ${moment(uptime).fromNow(true)}.`);
     } else if (msg.content.startsWith(prefix + "isdoe")) {
         let usr = msg.mentions.users.first();
         if (!isMention(usr)) {
             msg.channel.sendMessage(`${msg.author.username}, you need to mention a user to check if they're Doe.`);
+            return;
         } else if (usr.id === msg.author.id) {
             msg.channel.sendMessage(`I'd be concerned if you don't know who you are, ${msg.author.username}`);
-        } else if (usr.discriminator === "2753") {
+        } else if (usr.discriminator === "2753" || usr.discriminator === "6604") {
             msg.channel.sendMessage(`Yes, that is Doe, ${msg.author.username}`);
         } else {
             msg.channel.sendMessage(`Nope, that is not Doe, ${msg.author.username}`);
@@ -55,38 +60,68 @@ bot.on("message", (msg) => {
         let usr = msg.mentions.users.first();
         if (!isMention(usr)) {
             msg.channel.sendMessage(`${msg.author.username}, you need to mention a user to do whois.`);
+            return;
         } else {
             let theMember = msg.guild.member(usr);
             let dob = theMember.joinDate;
             let timez = moment(dob).fromNow();
             if (usr.id === msg.author.id) {
-                msg.channel.sendMessage(`${usr.username} (${usr.discriminator}), you joined ${timez}, on ${dob}.\nHere is your avatar: ${usr.avatarURL}`);
+                msg.channel.sendMessage(`${usr.username} (${usr.discriminator}), you joined ${timez}, on ${moment(dob).format("dddd, MMMM Do YYYY, h:mm:ss a (UTC Z)")}. Here is your avatar: ${usr.avatarURL}`);
             } else {
-                msg.channel.sendMessage(`${usr.username} (${usr.discriminator}) joined ${timez}, on ${dob}.\nHere is their avatar: ${usr.avatarURL}`);
+                msg.channel.sendMessage(`${usr.username} (${usr.discriminator}) joined ${timez}, on ${moment(dob).format("dddd, MMMM Do YYYY, h:mm:ss a (UTC Z)")}. Here is their avatar: ${usr.avatarURL}`);
             }
         }
-    } else if (msg.content.startsWith(prefix + "roll")) {
+    } else if (msg.content.startsWith(prefix + "roll") || msg.content.startsWith(prefix + "gmroll") ) {
         let cleanMsg = msg.cleanContent.replace(/!roll\s/,''); // remove !roll
-            cleanMsg = cleanMsg.replace(/[^0-9]+/g, ''); // remove anything that isnt a number . or ,
-        let sides = 6;
-        if (Number(cleanMsg) !== 0) {
-            sides = Number(cleanMsg);
+            cleanMsg = cleanMsg.replace(/[^0-9,]+/g, ''); // remove anything that isnt a number or ,
+        let args = cleanMsg.split(",");
+        args = args.filter(Number);
+        // default to one six-sided die with no modifier
+        let dice = 1, sides = 6, modifier = 0;
+        if (args.length !== 0) {
+            if (args.length > 3) {
+                msg.channel.sendMessage(`I detected too many numbers to roll, ${msg.author.username}. The command is !roll dice,sides,modifier, for example: !roll 2,6,5.`);
+                return;
+            } else if (args.length === 3){
+                dice = Number(args[0]);
+                sides = Number(args[1]);
+                modifier = Number(args[2]);
+                if (dice > 10) {
+                    msg.channel.sendMessage(`Too many dice, ${msg.author.username}. Maximum is 10.`);
+                    return;
+                }
+            } else if (args.length === 2) {
+                dice = Number(args[0]);
+                sides = Number(args[1]);
+                if (dice > 10) {
+                    msg.channel.sendMessage(`Too many dice, ${msg.author.username}. Maximum is 10.`);
+                    return;
+                }
+            } else if (args.length === 1) {
+                sides = Number(args[0]);
+            }
         }
-        let result = Math.floor(Math.random() * sides) + 1;
-        msg.channel.sendMessage(`${msg.author.username} rolled a ${result}`);
-    } else if (msg.content.startsWith(prefix + "gmroll")) {
-        let cleanMsg = msg.cleanContent.replace(/!gmroll\s/,''); // remove !gmroll
-            cleanMsg = cleanMsg.replace(/[^0-9]+/g, ''); // remove anything that isnt a number . or ,
-        let sides = 6;
-        if (Number(cleanMsg) !== 0) {
-            sides = Number(cleanMsg);
+        let rolls = [];
+        let str = "";
+        for (let i = 0; i < dice; i++) {
+            let roll = Math.floor(Math.random() * sides) + 1;
+            rolls[i] = roll;
+            str += "(" + roll + ") ";
         }
-        let result = Math.floor(Math.random() * sides) + 1;
-        msg.author.sendMessage(`You rolled a ${result}`);
-    } else if (msg.content.startsWith(prefix + "yn")) {
-        let rep;
-        ~~(Math.random()*2) ? rep = "Yes" : rep = "No";
-        msg.channel.sendMessage(`${rep}, ${msg.author.username}`);
+        var sum = rolls.reduce((a, b) => a + b, 0);
+        if (msg.content.startsWith(prefix + "roll")) {
+            msg.channel.sendMessage(`\`${str}+ ${modifier}\`\n${msg.author.username} rolled \*\*${sum+modifier}\*\*.`);
+        } else {
+            msg.author.sendMessage(`\`${str}+ ${modifier}\`\nYou rolled \*\*${sum+modifier}\*\*.`);
+        }
+    } else if (msg.content.startsWith(prefix + "yn") || msg.content.startsWith(prefix + "coin")) {
+        let result;
+        if (msg.content.startsWith(prefix + "yn")) {
+            ~~(Math.random()*2) ? result = "Yes" : result = "No";
+        } else {
+            ~~(Math.random()*2) ? result = "Heads" : result = "Tails";
+        }
+        msg.channel.sendMessage(`${result}, ${msg.author.username}`);
     } else if (msg.content.startsWith(prefix + "8ball")) {
         let answers = [
             'Maybe', 'Certainly not', 'I hope so', 'Not in your wildest dreams',
@@ -105,8 +140,10 @@ bot.on("message", (msg) => {
             array = array.filter(Number);
         if (array.length < 9 || array.length > 9) {
             msg.channel.sendMessage(`${msg.author.username}, I expected nine numbers in order to save your wellbeing scores. You gave me ${array.length}. Please try again.`);
+            return;
         } else if (array.some(isNaN)) {
             msg.channel.sendMessage(`${msg.author.username}, some of the data you gave me wasn't numbers. Please enter !setperma followed by nine comma seperated numbers.`);
+            return;
         } else {
             let timez = moment().format('x');
             let ref = db.ref(`${msg.guild.id}/perma/${msg.author.id}`);
@@ -131,6 +168,7 @@ bot.on("message", (msg) => {
         let usr = msg.mentions.users.first();
         if (!isMention(usr)) {
             msg.channel.sendMessage(`${msg.author.username}, you need to mention a user to get their PERMA scores.`);
+            return;
         } else {
             let ref = db.ref(`${msg.guild.id}/perma/${usr.id}`);
             ref.once("value", function(data){
@@ -186,9 +224,10 @@ bot.on("message", (msg) => {
             });
         }
     } else if (msg.content.startsWith(prefix + "setgoal")) {
-        let cleanMsg = msg.cleanContent.replace(/!setgoal\s/,'');
-        if (cleanMsg == "" || cleanMsg == null || !cleanMsg) {
+        let cleanMsg = msg.cleanContent.replace(/(^!setgoal)\s/i,'');
+        if (cleanMsg == "" || cleanMsg == null || !cleanMsg || cleanMsg == "!setgoal" || cleanMsg == "!setgoal ") {
             msg.channel.sendMessage(`${msg.author.username}, I didn't detect any text after you told me to set your goal. Please try again.`);
+            return;
         } else {
             let timestamp = moment().format('x');
             let ref = db.ref(`${msg.guild.id}/goals/`);
@@ -206,6 +245,7 @@ bot.on("message", (msg) => {
         let usr = msg.mentions.users.first();
         if (!isMention(usr)) {
             msg.channel.sendMessage(`${msg.author.username}, you need to mention a user to get their goals.`);
+            return;
         } else {
             let ref = db.ref(`${msg.guild.id}/goals/${usr.id}`);
             ref.once("value", function(data){
@@ -301,6 +341,7 @@ bot.on('warn', (w) => {
 });
 
 function init(){
+    uptime = moment();
     firebase.initializeApp({
         serviceAccount: "firebase.json",
         databaseURL: settings.db
